fdcf74a0e29d26cdaf0fe3f6de3af76d80576c04
Joseph Sepich <computerwhiz1337@gmail.com>
2022-06-13 13:41:45 -0400

    sprinkle some logging

--- main.go
+++ main.go
@@ -20,20 +20,25 @@ func ServeFib(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "no integer parameter n present", http.StatusBadRequest)
 		return
 	}
+	fmt.Println("Responding to Fibonacci request...")
 	if !ok {
+		fmt.Println("Error responding to Fibonacci request.")
 		http.Error(w, "error parsing integer parameter n", http.StatusBadRequest)
 		return
 	}
 	n, err := strconv.Atoi(values[0])
 	if err != nil {
+		fmt.Println("Error responding to Fibonacci request.")
 		http.Error(w, "error parsing integer parameter n", http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintln(w, fmt.Sprintf("Fibonacci number %d is %d", n, fib(n)))
+	fmt.Println("Responded to Fibonacci request.")
 }
 
 func main() {
 	http.HandleFunc("/", ServeFib)
 
+	fmt.Println("Starting web server on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
